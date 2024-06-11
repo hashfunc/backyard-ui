26a89a0a4aabe594cd5e7fc42901ee9c345dd1c0
hashfunc <root@hashfunc.io>
2024-06-11 21:36:56 +0900

    initial commit

---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+// biome-ignore lint: lint/style/noNonNullAssertion
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <p>backyard</p>
+  </React.StrictMode>,
+);
