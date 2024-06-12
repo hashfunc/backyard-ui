6a9afbe0146bc06e5bcfb5e68906f8303830e88d
hashfunc <root@hashfunc.io>
2024-06-12 23:06:58 +0900

    feat: Add RuleNode component

---
+++ src/components/RuleNode/RuleNode.stories.ts
@@ -0,0 +1,30 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import RuleNode from "./RuleNode";
+
+const meta = {
+  title: "RuleNode",
+  component: RuleNode,
+  parameters: {
+    layout: "centered",
+  },
+} satisfies Meta<typeof RuleNode>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    name: "Name of rule",
+    action: "ALLOW",
+    priority: 100,
+
+    domain: {
+      name: "Name of domain list",
+      count: 25,
+    },
+
+    status: "NORMAL",
+  },
+};
