c3c07b793ab13897480b27c99d148ccae5868fd9
hashfunc <root@hashfunc.io>
2024-08-24 11:50:47 +0900

    feat: Add global style

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,9 @@ import ReactDOM from "react-dom/client";
 
 import Welcome from "@/components/Welcome";
 
-import "./globals.css";
+import "@/styles/global.css";
+
+import "@/globals.css";
 
 // biome-ignore lint: lint/style/noNonNullAssertion
 ReactDOM.createRoot(document.getElementById("root")!).render(
